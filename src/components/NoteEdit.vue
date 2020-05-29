ab81e4831ba91695ff552119c365ee86a687ba4c
sean <zsean21598@163.com>
2020-05-29 20:45:06 +0800

    编辑框pc适配

--- src/components/NoteEdit.vue
+++ src/components/NoteEdit.vue
@@ -38,43 +38,6 @@
         </el-input>
       </main>
     </div>
-
-<!--    <div id="new-markdown"-->
-<!--         class="editor-layer">-->
-<!--      <div class="editor-top">-->
-<!--        <input class="editor-title form-control"-->
-<!--               type="text"-->
-<!--               placeholder="标题"-->
-<!--               v-model="note.title" />-->
-<!--        <div class="dropdown select-category">-->
-<!--          <button class="btn btn-default dropdown-toggle"-->
-<!--                  data-toggle="dropdown">-->
-<!--            <span class="category">{{this.$store.state.actionHelper.getCategoryName(note.categoryId)}}</span>-->
-<!--            <span class="caret"></span>-->
-<!--          </button>-->
-<!--          <ul class="dropdown-menu">-->
-<!--            <li @click="note.categoryId=0">-->
-<!--              <a>工作</a>-->
-<!--            </li>-->
-<!--            <li @click="note.categoryId=1">-->
-<!--              <a>生活</a>-->
-<!--            </li>-->
-<!--            <li @click="note.categoryId=2">-->
-<!--              <a>学习</a>-->
-<!--            </li>-->
-<!--          </ul>-->
-<!--        </div>-->
-<!--        <ul class="tools">-->
-<!--          <li @click="save" class="save"-->
-<!--             ><i class="iconfont icon-save"></i></li>-->
-<!--          <li @click="closeEditor" class="cancel"-->
-<!--              ><i class="iconfont icon-close"></i></li>-->
-<!--        </ul>-->
-<!--      </div>-->
-<!--      <textarea class="text-content form-control"-->
-<!--                placeholder="内容"-->
-<!--                v-model="note.content"></textarea>-->
-<!--    </div>-->
   </div>
 </template>
 
@@ -100,6 +63,7 @@ export default class NoteEdit extends Vue {
     note: ItemData = new ItemData(Category.Life,"","")
     created(){
       const editNote = this.$store.state.updatingNote;
+      console.log(1);
       if(editNote){
         this.note = editNote
       }
@@ -155,11 +119,30 @@ export default class NoteEdit extends Vue {
     box-shadow: 0 1px 3px rgba(0,0,0,.3);
     border-radius: 5px;
     @media screen and(min-width: 600px){
-      top: 20%;
+      top: 30%;
       left: 50%;
-      width: 60%;
+      width: 50%;
       transform: translate(-50%,-50%);
       min-height: 100px;
+      padding: 16px 20px;
+      header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+        height: 6%;
+      }
+      .title{
+        margin-bottom: 20px;
+      }
+      .el-textarea{
+        >textarea{
+          height: 200px;
+        }
+      }
+      .category-select{
+        width: 120px;
+      }
     }
     @media screen and(max-width: 600px) {
       top: auto;
@@ -175,7 +158,6 @@ export default class NoteEdit extends Vue {
         justify-content: space-between;
         width: 100%;
         height: 6%;
-        border-bottom: 1px solid rgba(0,0,0,0.1);
         padding: 0 15px;
       }
       .title{
@@ -185,9 +167,7 @@ export default class NoteEdit extends Vue {
         }
       }
       .el-textarea{
-        >textarea{
-          height: 80%;
-        }
+
       }
       .category-select{
         width: 120px;
